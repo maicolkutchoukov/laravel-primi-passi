36f3d698dc836eca236d4b95527384ec67f5593e
maicolkutchoukov <maicol.j.95@gmail.com>
2024-02-22 18:30:39 +0100

    Completato esercizio base

---
+++ resources/views/home.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laravel primi passi</title>
+</head>
+<body>
+    <header>
+        <h1>
+            I miei primi passi con Laravel
+        </h1>
+        <nav class="container">
+            <a href="#">
+                Scopri di piu..
+            </a>
+        </nav>
+    </header>
+</body>
+</html>
